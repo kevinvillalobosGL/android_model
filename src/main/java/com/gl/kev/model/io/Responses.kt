b9c370aea2fc6f8aa16a7570d896a2504ac9ea1a
Villalobos Kevin EXT <ext-Kevin.Villalobos@vaisala.com>
2019-03-21 13:30:06 -0600

    Add Data Manager (instead of Repository pattern) Add Api Calls

---
+++ src/main/java/com/gl/kev/model/io/Responses.kt
@@ -0,0 +1,12 @@
+package com.gl.kev.model.io
+
+import com.gl.kev.model.Photo
+import com.gl.kev.model.Todo
+
+class PhotoResponse : ArrayList<Photo>() {
+
+}
+
+class TodoResponse : ArrayList<Todo>() {
+
+}
